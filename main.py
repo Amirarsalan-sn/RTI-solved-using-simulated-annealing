c84007f95f6bffc6822eb1ab475f472812401186
Amir Arsalan Sanati <amirarsalan.sanati81@gmail.com>
2023-06-07 16:55:38 +0330

    first version

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ max_answer = cnf.nv
 temperature = len(cnf.clauses)
 t_coefficient = 0.8
 noise_possibility = 0.4
-n_coefficient = 0.9
+
+
+def convert_to_cnf(cnf_valuation):
+
+    return [(i+1 if cnf_valuation[i] == 1 else -(i+1)) for i in range(len(cnf_valuation))]
 
 
 def fitness(cnf_valuation):
@@ -20,31 +24,49 @@ def fitness(cnf_valuation):
     for parentheses in cnf.clauses:
         solver = Minisat22()
         solver.add_clause(parentheses)
-        if solver.solve(cnf_valuation):
+        if solver.solve(convert_to_cnf(cnf_valuation)):
             result += 1
     return result
 
 
 def add_noise(cnf_valuation):
     global noise_possibility
-    global n_coefficient
 
     for value in range(len(cnf_valuation)):
         random_number = random.randint(1, 10)
         if random_number <= noise_possibility:
             cnf_valuation[value] ^= 1
 
-    noise_possibility *= n_coefficient
+    return cnf_valuation
 
 
-def possibility(delta_f):
+def possibility(fitness1, fitness2):
     global temperature
-    global t_coefficient
-    pass
+
+    delta_f = math.fabs(fitness1 - fitness2)
+    result = math.exp(-delta_f / temperature)
+    return result
 
 
 def simulated_annealing(starting_point):
-    pass
+    print('process started')
+    for i in range(max_iteration):
+        while True:
+            temp_cnf_valuation = add_noise(cnf_valuation=starting_point)
+            fitness1 = fitness(starting_point)
+            fitness2 = fitness(temp_cnf_valuation)
+            if fitness1 < fitness2:
+                starting_point = temp_cnf_valuation
+                break
+            rand_num = random.random()
+            if rand_num <= possibility(fitness1, fitness2):
+                starting_point = temp_cnf_valuation
+                break
+
+        # checking the end conditions:
+        if fitness(starting_point) == max_answer:
+            break
+    print(starting_point)
 
 
 simulated_annealing(np.random.choice([0, 1], size=cnf.nv))
